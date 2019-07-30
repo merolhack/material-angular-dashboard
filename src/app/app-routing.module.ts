c8db9d66f453cb2247c21209654ee450fe658606
Lenin Meza <merolhack@gmail.com>
2019-07-30 00:06:37 -0500

    :lock: fix: canActivate property added to the app path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,19 +9,21 @@ import { DashboardComponent } from './pages/dashboard';
 import { Dashboard2Component } from './pages/dashboard2';
 import { FormsComponent } from './pages/forms';
 
+import { AuthGuard } from './services/auth';
+
 @NgModule({
   imports: [
     RouterModule.forRoot(
       [
         { path: '', redirectTo: 'app/dashboard', pathMatch: 'full' },
-        { path: 'app', component: CommonLayoutComponent, children: [
+        { path: 'app', component: CommonLayoutComponent, canActivate: [AuthGuard], children: [
           { path: 'dashboard', component: DashboardComponent, pathMatch: 'full' },
           { path: 'dashboard-custom', component: Dashboard2Component, pathMatch: 'full' },
           { path: 'forms', component: FormsComponent, pathMatch: 'full' },
           { path: 'charts', component: ChartsComponent, pathMatch: 'full' },
           { path: 'components', component: ComponentsComponent, pathMatch: 'full' },
           { path: '**', redirectTo: '/pages/404' },
-        ] }, // add 'canActivate: AuthGuard' for catching unauth users
+        ] },
         { path: 'ui', loadChildren: './pages/ui/ui.module#UIModule' },
         { path: 'maps', loadChildren: './pages/maps/maps.module#MapsModule' },
         { path: 'pages', loadChildren: './pages/pages/pages.module#PagesModule' },
